31ab16c61c9645ce4f658b41a9b8eefb89ceb7a8
Vyacheslav Koposov <s.koposek@gmail.com>
2024-02-07 18:23:25 +0300

    done

---
+++ medium/347-Top-K-Frequent-Elements.ts
@@ -0,0 +1,28 @@
+/**
+ * @link https://leetcode.com/problems/top-k-frequent-elements/description/
+ */
+
+function topKFrequent(nums: number[], k: number): number[] {
+
+  const hashTable = new Map<number, number>()
+
+  for (let i = 0; i < nums.length; i++) {
+    let count = hashTable.get(nums[i])
+    hashTable.set(nums[i], (count ?? 0) + 1)
+  }
+
+  const newArr: number[] = []
+  hashTable.forEach((value, key) => {
+    if (value >= k) {
+      newArr.push(key)
+    }
+  })
+
+  console.log(newArr)
+  return newArr
+}
+
+// const nums = [1, 1, 1, 2, 2, 3], k = 2
+const nums = [1,2], k = 2
+topKFrequent(nums, k)
+// Output: [1,2]
