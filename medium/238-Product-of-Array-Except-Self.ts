d1b80713b7e72549657cebd260c9da5a704a7d9b
Vyacheslav Koposov <s.koposek@gmail.com>
2024-02-09 18:11:12 +0300

    donne

---
+++ medium/238-Product-of-Array-Except-Self.ts
@@ -0,0 +1,22 @@
+/**
+ *
+ * @link https://leetcode.com/problems/product-of-array-except-self/
+ */
+function productExceptSelf(
+  nums: number[]
+): number[] {
+
+  const hash = new Map<number, number[>()
+
+  for (let i = 0; i < nums.length; i++) {
+
+  }
+
+  return []
+}
+
+let nums = [1, 2, 3, 4]
+// Вывод: [24,12,8,6]
+productExceptSelf(nums)
+// nums = [-1,1,0,-3,3]
+// Выход: [0,0,9,0,0]
--- medium/347-Top-K-Frequent-Elements.ts
+++ medium/347-Top-K-Frequent-Elements.ts
@@ -8,21 +8,45 @@ function topKFrequent(nums: number[], k: number): number[] {
 
   for (let i = 0; i < nums.length; i++) {
     let count = hashTable.get(nums[i])
-    hashTable.set(nums[i], (count ?? 0) + 1)
+    const value = (count ?? 0) + 1
+    hashTable.set(nums[i], value)
   }
 
-  const newArr: number[] = []
-  hashTable.forEach((value, key) => {
-    if (value >= k) {
-      newArr.push(key)
-    }
-  })
+  const freq: Record<number, number[]> & Object = {}
 
-  console.log(newArr)
-  return newArr
+  for (const [key, value] of hashTable.entries()) {
+    const values = freq[value] ?? []
+    freq[value] = values.concat(key)
+  }
+  const res: number[] = []
+
+  for (let i = Object.keys(nums).length; i >= 0; i--) {
+    const value = freq[i]
+    if (!value) continue;
+    res.push(...value)
+    const countRes = res.length
+
+    if (countRes == k) break;
+
+    if (countRes > k) {
+      deleteLastElement(res, k)
+    }
+  }
+
+
+  return res
 }
 
+const deleteLastElement = (arr: number[], k: number) => {
+  arr.pop()
+  if (arr.length > k) {
+    deleteLastElement(arr, k)
+  }
+}
+
+
 // const nums = [1, 1, 1, 2, 2, 3], k = 2
-const nums = [1,2], k = 2
-topKFrequent(nums, k)
+const nums1 = [1, 2, 5, 5, 5], k = 2
+const nums2 = [1], k2 = 1
+topKFrequent(nums2, k2)
 // Output: [1,2]
